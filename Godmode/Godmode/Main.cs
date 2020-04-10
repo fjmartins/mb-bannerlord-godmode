5a0d0878bc0046ebd3fe84ae047d03defb96a8ca
fjmartins <fabiomsjunior@gmail.com>
2020-04-10 23:10:33 +0900

    godmode

---
+++ Godmode/Godmode/Main.cs
@@ -0,0 +1,14 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace Godmode {
+    public class Main : MBSubModuleBase {
+        protected override void OnApplicationTick(float dt) {
+            Agent agent = Agent.Main;
+
+            if (agent != null) {
+                // Just in case you get impaled by a huge spear
+                agent.Health = agent.HealthLimit * 1000;
+            }
+        }
+    }
+}

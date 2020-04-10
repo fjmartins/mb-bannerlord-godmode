ab35f629f6f67c3fe2bc9bbe0fcf12caa28d39d6
fjmartins <fabiomsjunior@gmail.com>
2020-04-10 23:37:33 +0900

    fix

---
+++ Godmode/Godmode/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+
+namespace Godmode {
+    public class Main : MBSubModuleBase {
+        protected override void OnApplicationTick(float dt) {
+            Agent agent = Agent.Main;
+
+            if (agent != null) {
+                // Just in case you get impaled by a huge spear
+                agent.Health = agent.HealthLimit * 1000;
+            }
+        }
+    }
+}
